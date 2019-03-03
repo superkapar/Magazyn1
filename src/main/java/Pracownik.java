c5d6ac50886daee6700ab5c2a0e935abf4395764
superkapar <kapceru@gmail.com>
2019-03-03 15:30:42 +0100

    asd

--- src/main/java/Pracownik.java
+++ src/main/java/Pracownik.java
@@ -1,2 +1,9 @@
 public class Pracownik {
+    String imie;
+    String nazwisko;
+    String pesel;
+    int wiek;
+    double zarobki;
 }
+
+
--- src/main/java/Produkt.java
+++ src/main/java/Produkt.java
@@ -1,2 +1,7 @@
 public class Produkt {
+    int cena;
 }
+
+
+ //   Stwórz klasę Produkt (nazwa, cena netto, podatek vat).
+
